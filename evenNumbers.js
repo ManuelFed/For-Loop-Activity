dbf45e28b4f5da8aa1417fa74fd40e1df925f0f2
ManuelFed <mftudayan21@gmail.com>
2025-03-09 15:47:03 +0800

    git commit -m

---
+++ evenNumbers.js
@@ -0,0 +1,23 @@
+// Name: Manuel Federico D. Tudayan
+// Activity: Perf2: Conditional Statements and Loops
+// Data Structures and Algorithms
+
+// FOR LOOP ACTIVITY:
+
+// For Loop
+// Create a for loop to display the numbers 1 through 10.
+// Initialize the variable "i" to 1, if less than or equal to 10, then increment by 1.
+for (let i = 1; i <= 10; i++) {
+  // Checks if the number(i) is even, then displays the number(i), if true.
+  if (i % 2 == 0) {
+    console.log(i);
+    /*
+    Output:
+    2
+    4
+    6
+    8
+    10
+    */
+  }
+}
